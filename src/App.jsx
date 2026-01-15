a1423aa706aecc9746d1d926ec3b4d0ed4a35dcf
Fachrzhmd <rizagaming0412@gmail.com>
2026-01-15 13:27:07 +0700

    update App h1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-green-400">
-        Tailwind CSS Berhasils 🎉
+        Bismillah
       </h1>
     </div>
   );
