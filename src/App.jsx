b53f8d59ae979df637772655ef621928dbef8fa0
Fachrzhmd <rizagaming0412@gmail.com>
2026-01-15 11:52:21 +0700

    initial react monitoring project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind CSS Berhasil 🎉
+      </h1>
+    </div>
+  )
+}
