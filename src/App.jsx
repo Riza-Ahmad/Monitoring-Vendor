bbd6f03beb4c6ef2cd982b509d1a355b6d636287
gaisha cantik <gaisha cantik>
2026-01-15 14:52:44 +0700

    berubah coy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-green-400">
-        Bismillah
+        change caption
       </h1>
     </div>
   );
