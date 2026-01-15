8c8da0d35273626f4e8fe805444e90933a6f10f4
Fachrzhmd <rizagaming0412@gmail.com>
2026-01-15 13:19:29 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-green-400">
-        Tailwind CSS Berhasil 🎉
+        Tailwind CSS Berhasils 🎉
       </h1>
     </div>
-  )
+  );
 }
