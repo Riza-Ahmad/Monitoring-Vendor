c31d059bb9568b0dd6e8962e61adea0eb4f6ec80
Fachrzhmd <rizagaming0412@gmail.com>
2026-01-19 10:11:29 +0700

    ambil sidebar darri gesa

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 import Footer from "../../Monitoring-Vendor/src/components/Footer";
 import Navbar from "../../Monitoring-Vendor/src/components/Navbar";
+import Sidebar from "../../Monitoring-Vendor/src/components/Sidebar";
 
 const OnboardingPage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col">
       <Navbar />
+      <Sidebar />
       <main className="flex-grow"></main>
 
       <Footer />
